5d24c4cdbe6dc0d0cc3e7604dfa4d4d1e393abf4
向井優一 <yuichi-m@weseek.co.jp>
2021-06-11 19:59:04 +0900

    add modal user-detail-view

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
-import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react'
-import { memo, useEffect, VFC } from 'react'
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react'
+import { memo, useCallback, useEffect, VFC } from 'react'
 
 import { UserCard } from '../organisms/user/UserCard'
 import { useAllUsers } from '../../hooks/useAllUsers'
+import { UserDetailModal } from '../organisms/user/UserDetailModal'
+import { useSelectUser } from '../../hooks/useSelectUser'
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { getUsers, users, loading } = useAllUsers()
+  const { onSelectUser, selectedUser } = useSelectUser()
 
   useEffect(() => getUsers(), [])
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen })
+    },
+    [users, onSelectUser, onOpen],
+  )
+
   return (
     <>
       {loading ? (
@@ -21,14 +38,17 @@ export const UserManagement: VFC = memo(() => {
           {users.map((user) => (
             <WrapItem key={user.id} mx="auto">
               <UserCard
+                id={user.id}
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
     </>
   )
 })
