2e37ccf85c5ddec2f461bf26b8d7c1ff053bf155
向井優一 <yuichi-m@weseek.co.jp>
2021-06-11 21:51:57 +0900

    complete prototype app

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -12,11 +12,13 @@ import { UserCard } from '../organisms/user/UserCard'
 import { useAllUsers } from '../../hooks/useAllUsers'
 import { UserDetailModal } from '../organisms/user/UserDetailModal'
 import { useSelectUser } from '../../hooks/useSelectUser'
+import { useLoginUser } from '../../hooks/useLoginUser'
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { getUsers, users, loading } = useAllUsers()
   const { onSelectUser, selectedUser } = useSelectUser()
+  const { loginUser } = useLoginUser()
 
   useEffect(() => getUsers(), [])
 
@@ -48,7 +50,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        onClose={onClose}
+        isAdmin={loginUser?.isAdmin}
+      />
     </>
   )
 })
